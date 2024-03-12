347696796887a4b5ce440384b56a66044d6cc131
PERSONIV\rico.bugtong <rico.gomez.bugtong@gmail.com>
2024-03-12 20:31:39 +0800

    personiv apps

---
+++ resources/views/index.blade.php
@@ -0,0 +1,78 @@
+<!--
+
+███╗   ██╗██╗██╗  ██╗██╗  ██╗ ██████╗     ███████╗ █████╗ ██████╗  █████╗ ██╗      █████╗
+████╗  ██║██║██║ ██╔╝██║ ██╔╝██╔═══██╗    ╚══███╔╝██╔══██╗██╔══██╗██╔══██╗██║     ██╔══██╗
+██╔██╗ ██║██║█████╔╝ █████╔╝ ██║   ██║      ███╔╝ ███████║██████╔╝███████║██║     ███████║
+██║╚██╗██║██║██╔═██╗ ██╔═██╗ ██║   ██║     ███╔╝  ██╔══██║██╔══██╗██╔══██║██║     ██╔══██║
+██║ ╚████║██║██║  ██╗██║  ██╗╚██████╔╝    ███████╗██║  ██║██████╔╝██║  ██║███████╗██║  ██║
+╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝     ╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                                          
+visit me at : https://www.nikkozabala.com
+or email at : nikko.zabala@gmail.com
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" sizes="16x16" href="{{ asset('assets/img/favicon.ico') }}">
+    <title>Personiv Apps</title>
+
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Days+One' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Style css -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+
+    <header class="header">
+        <hgroup>
+            <a title="Click here" href="it.html">
+                <h1 class="p-apps">PERSONIV APPS</h1>
+
+                <small class="itd">Information & Technology Department</small>
+            </a>
+        </hgroup>
+    </header>
+
+    <section class="demo">
+
+        <dl class="list nigiri">
+            <dt class="dt-title">Portals</dt>
+            <dd><a target="_blank" href="https://web.personiv.com/hrportal">HR Portal</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/personivsign">Personiv Sign</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/pqr">ONLINE PQR</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/facilities-portal">Facilities Portal</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/service-desk/public">Service Desk</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/reservation-portal/public">Reservation Portal</a></dd>
+            <dd><a target="_blank"
+                    href="https://account.activedirectory.windowsazure.com/applications/signin/51e7d381-bdeb-47d1-9d8b-583dce65f7eb?tenantId=4a811458-0fdc-4768-acec-1182b80917ee">Oracle</a>
+            </dd>
+        </dl>
+
+        <dl class="list maki">
+            <dt class="dt-title">Department Apps</dt>
+            <dd><a target="_blank" href="https://web.personiv.com/lamar">Lamar Work Flow</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/weblog">Weblog</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/kbt-v2">CS Knowledge Based Tool</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/dco-scorecard/public">DCO Scorecard</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/dms">Document Management</a></dd>
+            <dd><a target="_blank" href="https://faowbtl.personiv.com/fao-tasklists-v2/public/">FAO Tasklists</a></dd>
+        </dl>
+
+        <dl class="list sashimi">
+            <dt class="dt-title">Employee Engagement</dt>
+            <dd><a target="_blank" href="https://web.personiv.com/aims-v2/public">AIMS</a></dd>
+            <dd><a target="_blank" href="https://web.personiv.com/erg-portal">ERG Portal</a></dd>
+        </dl>
+    </section>
+</body>
+
+</html>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
